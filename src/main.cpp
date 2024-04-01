709ae121c9260fddd0a7e261fc8a33b1f59b27cf
Benjamin2her <benjamin.hernandez.herrera@uabc.edu.mx>
2024-04-01 15:09:37 -0700

    ADD blinking led code in the main.cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,26 @@
 #include <Arduino.h>
 
+//Blinking LED and serial communicaction
+
+int pinLed = 8; //pin donde se conecta el led
 void setup() {
-    // put your setup code here, to run once:
+    
+    // Inicializa el LED apagado
+    pinMode(pinLed, OUTPUT);
+    digitalWrite(pinLed, LOW);
+
+    // Inicializa la consola serial
     Serial.begin(9600);
-    Serial.print("Hola, este es mi primer mensaje en la consola");
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+    // Enciende el led por 1 segundo e imprime el estado en la consola serial
+    Serial.println("LED is turned on");
+    digitalWrite(pinLed, HIGH);
+    delay(1000);
+
+    // Apaga el led por 1 segundo e imprime el estado en la consola serial
+    Serial.println("LED is turned off");
+    digitalWrite(pinLed, LOW);
+    delay(1000);
 }
