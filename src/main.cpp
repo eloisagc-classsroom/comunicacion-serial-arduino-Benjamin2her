a36891e3ac029781da326e43a44facb0edaee9a2
Benjamin2her <benjamin.hernandez.herrera@uabc.edu.mx>
2024-04-01 14:38:10 -0700

    Creaación de Proyecto - Comunicación Serial

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+void setup() {
+    // put your setup code here, to run once:
+    Serial.begin(9600);
+    Serial.print("Hola, este es mi primer mensaje en la consola");
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+}
